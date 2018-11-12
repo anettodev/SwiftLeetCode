a0ceb25acab8dab4a6d5dfa1d9e68552b666ad6f
Antonio Netto <anetto@avenuecode.com>
2018-11-11 22:32:16 -0200

    Two Sum Solution

---
+++ [LeetCode-001]TwoSum.playground/Contents.swift
@@ -0,0 +1,29 @@
+import UIKit
+/*
+ 001. Two Sum
+ ---------------------------------------------------------------------------------------------
+ Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+ 
+ You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ 
+ Example:
+ 
+ Given nums = [2, 7, 11, 15], target = 9,
+ 
+ Because nums[0] + nums[1] = 2 + 7 = 9,
+ return [0, 1].
+ ---------------------------------------------------------------------------------------------
+ difficulty:easy
+ source: https://leetcode.com/problems/two-sum/
+ */
+
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    for i in 0..<nums.count {
+        for j in i+1..<nums.count {
+            if nums[i] + nums[j] == target {
+                return [i,j]
+            }
+        }
+    }
+    return []
+}
